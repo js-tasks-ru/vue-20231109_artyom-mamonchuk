3f47e46467dee71eb09f80678eb84bdf98396e5a
Artem <mart_22@ukr.net>
2023-11-01 00:08:17 +0200

    Solve vue calculator task

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,25 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+  data() {
+    return {
+      value1: 0,
+      value2: 0,
+      operator: 'sum',
+    };
+  },
+  computed: {
+    sum() {
+      switch (this.operator) {
+        case 'sum':
+          return this.value1 + this.value2;
+        case 'subtract':
+          return this.value1 - this.value2;
+        case 'multiply':
+          return this.value1 * this.value2;
+        default:
+          return this.value1 / this.value2;
+      }
+    },
+  },
+}).mount('#app');
