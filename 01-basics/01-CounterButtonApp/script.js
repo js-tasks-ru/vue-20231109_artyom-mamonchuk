905081c481e97d535ef4b29a718556e5d59eab6d
Artem <mart_22@ukr.net>
2023-10-31 23:33:40 +0200

    Solve counter button app task

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,14 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    counterIncrement() {
+      this.counter++;
+    },
+  },
+}).mount('#app');
