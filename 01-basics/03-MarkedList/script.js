986d73e34320da8d90fe4acbba532262ecc75eb2
Artem <mart_22@ukr.net>
2023-11-02 21:28:23 +0200

    Solve task MarkedList

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,32 +1,64 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// From https://jsonplaceholder.typicode.com/comments
-const emails = [
-  'Eliseo@gardner.biz',
-  'Jayne_Kuhic@sydney.com',
-  'Nikita@garfield.biz',
-  'Lew@alysha.tv',
-  'Hayden@althea.biz',
-  'Presley.Mueller@myrl.com',
-  'Dallas@ole.me',
-  'Mallory_Kunze@marie.org',
-  'Meghan_Littel@rene.us',
-  'Carmen_Keeling@caroline.name',
-  'Veronica_Goodwin@timmothy.net',
-  'Oswald.Vandervort@leanne.org',
-  'Kariane@jadyn.tv',
-  'Nathan@solon.io',
-  'Maynard.Hodkiewicz@roberta.com',
-  'Christine@ayana.info',
-  'Preston_Hudson@blaise.tv',
-  'Vincenza_Klocko@albertha.name',
-  'Madelynn.Gorczany@darion.biz',
-  'Mariana_Orn@preston.org',
-  'Noemie@marques.me',
-  'Khalil@emile.co.uk',
-  'Sophia@arianna.co.uk',
-  'Jeffery@juwan.us',
-  'Isaias_Kuhic@jarrett.net',
-];
+createApp({
+  data() {
+    return {
+      email: '',
+      emails: [],
+      visible: false,
+    };
+  },
+  mounted() {
+    this.getEmails();
+  },
+  computed: {
+    filtered() {
+      return this.emails.filter((email) => email.toLowerCase().trim().includes(this.email.toLowerCase().trim()));
+    },
+  },
+  methods: {
+    getEmails() {
+      fetch('https://jsonplaceholder.typicode.com/comments')
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          data.forEach((comment) => {
+            if (comment?.email) {
+              this.emails.push(comment?.email);
+            }
+          });
+        })
+        .catch((error) => {
+          console.error(error?.message);
+        });
+    },
+    toggleVisible() {
+      this.visible = !this.visible;
+    },
+    func(userEmail) {
+      const reg = new RegExp(`${this.email}`, 'gi');
 
-// Требуется создать Vue приложение
+      if (this.email !== '') {
+        if (userEmail.search(reg) == -1) {
+          return userEmail;
+        } else {
+          this.visible = true;
+          return this.insertMark(userEmail, userEmail.search(reg), this.email.length);
+        }
+      } else {
+        this.visible = false;
+        return userEmail;
+      }
+    },
+    insertMark(string, position, len) {
+      return (
+        string.slice(0, position) +
+        '<span>' +
+        string.slice(position, position + len) +
+        '</span>' +
+        string.slice(position + +len)
+      );
+    },
+  },
+}).mount('#app');
