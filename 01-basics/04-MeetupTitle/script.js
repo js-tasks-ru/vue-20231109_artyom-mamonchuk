e1c769f79017c379da03be80f8f86a8ac652a697
Artem <mart_22@ukr.net>
2023-11-03 00:21:02 +0200

    Solve task MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,32 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
-      });
-    }
-  });
-}
-
-// Требуется создать Vue приложение
+createApp({
+  data() {
+    return {
+      id: 1,
+      meetupTitle: '',
+    };
+  },
+  created() {
+    this.fetchMeetupById(this.id);
+  },
+  methods: {
+    fetchMeetupById(meetupId) {
+      fetch(`${API_URL}/meetups/${meetupId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          this.meetupTitle = data?.title;
+        })
+        .catch((error) => {
+          console.error(error?.message);
+        });
+    },
+  },
+  watch: {
+    id(newId, oldId) {
+      this.fetchMeetupById(this.id);
+    },
+  },
+}).mount('#app');
