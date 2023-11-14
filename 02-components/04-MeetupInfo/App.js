863cb3893c13780f3892f8818265337a089180b7
Artem <mart_22@ukr.net>
2023-11-14 19:56:24 +0200

    02-components/04-MeetupInfo: Solve task

--- 02-components/04-MeetupInfo/App.js
+++ 02-components/04-MeetupInfo/App.js
@@ -11,26 +11,24 @@ export default defineComponent({
 
   data() {
     return {
+      title: 'MeetupInfo',
       meetups,
       selectedMeetup: 0,
     };
   },
 
   template: `
-    <div class="sample container">
-      <p>
-        <select v-model="selectedMeetup">
-          <option v-for="(meetup, index) in meetups" :key="meetup.id" :value="index">
+   <div class="container pt-5 pb-5">
+  		<h1 class="mb-4">{{ title }}</h1>
+      <select v-model="selectedMeetup" class="form-select mb-4">
+         <option v-for="(meetup, index) in meetups" :key="meetup.id" :value="index">
             {{ index }}: {{ meetup.title }}
-          </option>
-        </select>
-      </p>
-
-      <MeetupInfo
+         </option>
+      </select>
+      <meetup-info
         :organizer="meetups[selectedMeetup].organizer"
         :place="meetups[selectedMeetup].place"
-        :date="meetups[selectedMeetup].date"
-      />
-    </div>
+        :date="meetups[selectedMeetup].date"></meetup-info>
+   </div>
   `,
 });
