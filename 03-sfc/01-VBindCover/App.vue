5d99aa976cb38ae9c88f6d88eb8ee606b975a683
Artem <mart_22@ukr.net>
2023-11-20 13:46:51 +0200

    03-sfc/01-VBindCover: Solve task

--- 03-sfc/01-VBindCover/App.vue
+++ 03-sfc/01-VBindCover/App.vue
@@ -1,35 +1,27 @@
 <template>
-  <div class="sample">
-    <h1 style="text-align: center">С изображением</h1>
-    <MeetupCover :title="meetupWithImage.title" :image="meetupWithImage.image" />
+	<div class="container pt-5 pb-5">
+		<h1 style="text-align: center" class="mb-3">С изображением</h1>
+		<MeetupCover :title="meetupWithImage.title" :image="meetupWithImage.image"></MeetupCover>
 
-    <h1 style="text-align: center">Без изображения</h1>
-    <MeetupCover :title="meetupWithoutImage.title" :image="meetupWithoutImage.image" />
-  </div>
+		<h1 style="text-align: center" class="mb-3">Без изображения</h1>
+		<MeetupCover :title="meetupWithoutImage.title" :image="meetupWithoutImage.image"></MeetupCover>
+	</div>
 </template>
 
-<script>
+<script setup>
 import MeetupCover from './components/MeetupCover.vue';
 
-export default {
-  name: 'App',
-
-  components: { MeetupCover },
-
-  data() {
-    return {
-      meetupWithImage: {
-        title: 'VueConf US',
-        image: 'https://course-vue.javascript.ru/api/images/5',
-      },
-
-      meetupWithoutImage: {
-        title: 'Demo meetup',
-        image: null,
-      },
-    };
-  },
+const meetupWithImage = {
+	title: 'VueConf US',
+	image: 'https://course-vue.javascript.ru/api/images/5',
 };
+
+const meetupWithoutImage = {
+	title: 'Demo meetup',
+	image: null,
+};
+
 </script>
 
-<style></style>
+<style>
+</style>
