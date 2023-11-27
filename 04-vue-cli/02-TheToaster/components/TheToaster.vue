3d5748f63f0ac8a10c62f19adba9a59a8db3519f
Artem <mart_22@ukr.net>
2023-11-27 23:49:32 +0200

    Revert " 04-vue-cli/02-TheToaster: Solve task"
    This reverts commit 18b122f8df23ae78c4255bc53cf6cc4d32d180ed.

--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -1,53 +1,73 @@
 <template>
-	<div class="toasts">
-		<UiToast v-for="(toaster, index) in toasters" :key="toaster.id" :toster-status="toaster.status"
-			:toster-icon="toaster.icon" @delete-toast="$emit('delete-toast', index)">
-			<span>{{ toaster.text }} {{ toaster.timestamp }}</span>
-		</UiToast>
-	</div>
+  <div class="toasts">
+    <div class="toast toast_success">
+      <UiIcon class="toast__icon" icon="check-circle" />
+      <span>Success Toast Example</span>
+    </div>
+
+    <div class="toast toast_error">
+      <UiIcon class="toast__icon" icon="alert-circle" />
+      <span>Error Toast Example</span>
+    </div>
+  </div>
 </template>
 
 <script>
 import UiIcon from './UiIcon.vue';
-import UiToast from './UiToast.vue';
 
 export default {
-	name: 'TheToaster',
+  name: 'TheToaster',
 
-	components: { UiIcon, UiToast },
-
-	mounted()
-	{
-
-	},
-
-	emits: ['delete-toast'],
-
-	props: {
-		toasters: {
-			type: Array,
-			required: true
-		}
-	},
+  components: { UiIcon },
 };
 </script>
 
-<style scoped lang="scss">
+<style scoped>
 .toasts {
-	position: fixed;
-	bottom: 8px;
-	right: 8px;
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-end;
-	white-space: pre-wrap;
-	z-index: 999;
+  position: fixed;
+  bottom: 8px;
+  right: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  white-space: pre-wrap;
+  z-index: 999;
 }
 
 @media all and (min-width: 992px) {
-	.toasts {
-		bottom: 72px;
-		right: 112px;
-	}
+  .toasts {
+    bottom: 72px;
+    right: 112px;
+  }
+}
+
+.toast {
+  display: flex;
+  flex: 0 0 auto;
+  flex-direction: row;
+  align-items: center;
+  padding: 16px;
+  background: #ffffff;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+  font-size: 18px;
+  line-height: 28px;
+  width: auto;
+}
+
+.toast + .toast {
+  margin-top: 20px;
+}
+
+.toast__icon {
+  margin-right: 12px;
+}
+
+.toast.toast_success {
+  color: var(--green);
+}
+
+.toast.toast_error {
+  color: var(--red);
 }
 </style>
