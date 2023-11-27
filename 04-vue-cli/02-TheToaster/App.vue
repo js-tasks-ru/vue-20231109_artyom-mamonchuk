af96ab87cd64d94a3e1fc76f208bfc555979e2ff
Artem <mart_22@ukr.net>
2023-11-27 23:53:19 +0200

    Revert " 04-vue-cli/02-TheToaster: Solve task"
    This reverts commit 18b122f8df23ae78c4255bc53cf6cc4d32d180ed.

--- 04-vue-cli/02-TheToaster/App.vue
+++ 04-vue-cli/02-TheToaster/App.vue
@@ -1,98 +1,27 @@
 <template>
-	<div class="toaster-controls">
-		<button type="button" class="btn btn-success" @click="handleClick('success')">Add Success Message</button>
-		<button type="button" class="btn btn-danger" @click="handleClick('error')">Add Error Message</button>
-		<button type="button" class="btn btn-info" @click="handleClick('info')">Add Info Message</button>
-		<button type="button" class="btn btn-warning" @click="handleClick('warning')">Add Warning Message</button>
-	</div>
-	<TheToaster v-if="toasters.length" :toasters="toasters" @delete-toast="handleDeleteToast" />
+  <TheToaster ref="toaster" />
+  <p>
+    <button type="button" @click="handleSuccessClick">Add Success Message</button>
+    <button type="button" @click="handleErrorClick">Add Error Message</button>
+  </p>
 </template>
 
 <script>
 import TheToaster from './components/TheToaster.vue';
 
 export default {
-	name: 'App',
+  name: 'App',
 
-	components: { TheToaster },
+  components: { TheToaster },
 
-	toastersText: [
-		{
-			status: 'success',
-			text: 'Success',
-			icon: 'check-circle'
-		},
-		{
-			status: 'error',
-			text: 'Error',
-			icon: 'alert-circle'
-		},
-		{
-			status: 'info',
-			text: 'Info',
-			icon: 'chevron-down'
-		},
-		{
-			status: 'warning',
-			text: 'Warning',
-			icon: 'trash'
-		}
-	],
+  methods: {
+    handleSuccessClick() {
+      this.$refs.toaster.success('Success ' + new Date().toLocaleTimeString());
+    },
 
-	data()
-	{
-		return {
-			toasters: [],
-		}
-	},
-
-	methods: {
-		handleClick(status)
-		{
-			const toasterFiltered = (this.$options.toastersText.filter(el => el.status === status));
-			const [toast] = toasterFiltered;
-
-			const newToaster = {
-				id: Date.now(),
-				timestamp: new Date().toLocaleTimeString(),
-				status: toast.status,
-				text: toast.text,
-				icon: toast.icon
-			}
-
-			this.toasters.push(newToaster);
-		},
-		handleDeleteToast(index)
-		{
-			this.toasters.splice(index, 1);
-
-		},
-		autoDeleteToast()
-		{
-			setTimeout(() =>
-			{
-				this.toasters.shift();
-			}, 5000);
-		}
-	},
-	watch: {
-		toasters: {
-			handler()
-			{
-				this.autoDeleteToast();
-			},
-			deep: true
-		},
-	}
+    handleErrorClick() {
+      this.$refs.toaster.error('Error ' + new Date().toLocaleTimeString());
+    },
+  },
 };
 </script>
-
-<style scoped lang="scss">
-.toaster-controls {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-wrap: wrap;
-	gap: 10px;
-}
-</style>
