5a38637891c762a793108e751a503693c091e913
Artem <mart_22@ukr.net>
2023-12-24 20:18:38 +0200

    07-forms/01-CounterButtonComplete: Solve task

--- 07-forms/01-CounterButtonComplete/App.vue
+++ 07-forms/01-CounterButtonComplete/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <div id="app" class="sample container">
-    <p><CounterButton v-model:count="count" /> - count = {{ count }}</p>
-    <p><CounterButton /> - независимый счётчик</p>
-  </div>
+	<div id="app" class="pt-5 pb-5 container">
+		<p>
+			<CounterButton class="btn btn-primary" v-model:count="count" /> - count = {{ count }}
+		</p>
+		<p>
+			<CounterButton class="btn btn-primary" v-model:count="count" /> - независимый счётчик
+		</p>
+	</div>
 </template>
 
 <script>
 import CounterButton from './components/CounterButton.vue';
 
 export default {
-  name: 'App',
-  components: { CounterButton },
-  data() {
-    return {
-      count: 1,
-    };
-  },
+	name: 'App',
+
+	components: { CounterButton },
+
+	data()
+	{
+		return {
+			count: 1,
+		};
+	},
 };
 </script>
 
