97250bd4256fc382f7edcd905e332d4df6360d06
Artem <mart_22@ukr.net>
2023-12-26 01:06:51 +0200

    07-forms/02-MeetupForm: Solve task

--- 07-forms/02-MeetupForm/components/MeetupAgendaItemForm.vue
+++ 07-forms/02-MeetupForm/components/MeetupAgendaItemForm.vue
@@ -1,46 +1,26 @@
 <!-- STUB: ЭТО ЗАГЛУШКА ДЛЯ РУЧНОГО ТЕСТИРОВАНИЯ -->
 
 <template>
-  <fieldset class="agenda-item-form">
-    <button type="button" class="agenda-item-form__remove-button" @click="$emit('remove')">
-      <UiIcon icon="trash" />
-    </button>
+	<fieldset class="agenda-item-form">
+		<button type="button" class="agenda-item-form__remove-button" @click="$emit('remove')">
+			<UiIcon icon="trash" />
+		</button>
 
-    <div class="agenda-item-form__row">
-      <div class="agenda-item-form__col">
-        <UiFormGroup label="Начало">
-          <UiInput
-            name="startsAt"
-            type="time"
-            placeholder="00:00"
-            :model-value="agendaItem.startsAt"
-            @change="
-              $emit('update:agendaItem', {
-                ...agendaItem,
-                startsAt: $event.target.value,
-              })
-            "
-          />
-        </UiFormGroup>
-      </div>
-      <div class="agenda-item-form__col">
-        <UiFormGroup label="Окончание">
-          <UiInput
-            name="endsAt"
-            type="time"
-            placeholder="00:00"
-            :model-value="agendaItem.endsAt"
-            @change="
-              $emit('update:agendaItem', {
-                ...agendaItem,
-                endsAt: $event.target.value,
-              })
-            "
-          />
-        </UiFormGroup>
-      </div>
-    </div>
-  </fieldset>
+		<div class="agenda-item-form__row">
+			<div class="agenda-item-form__col">
+				<UiFormGroup label="Начало">
+					<UiInput name="startsAt" type="time" placeholder="00:00" :model-value="agendaItem.startsAt"
+						@change="setAgendaItemField($event.target.name, $event.target.value)" />
+				</UiFormGroup>
+			</div>
+			<div class="agenda-item-form__col">
+				<UiFormGroup label="Окончание">
+					<UiInput name="endsAt" type="time" placeholder="00:00" :model-value="agendaItem.endsAt"
+						@change="setAgendaItemField($event.target.name, $event.target.value)" />
+				</UiFormGroup>
+			</div>
+		</div>
+	</fieldset>
 </template>
 
 <script>
@@ -49,86 +29,97 @@ import UiIcon from './UiIcon.vue';
 import UiInput from './UiInput.vue';
 
 export default {
-  name: 'MeetupAgendaItemForm',
+	name: 'MeetupAgendaItemForm',
 
-  components: { UiInput, UiIcon, UiFormGroup },
+	components: { UiInput, UiIcon, UiFormGroup },
 
-  props: {
-    agendaItem: {
-      type: Object,
-      required: true,
-    },
-  },
+	props: {
+		agendaItem: {
+			type: Object,
+			required: true,
+		},
+		count: {
+			type: Number,
+			default: 0
+		}
+	},
 
-  emits: ['update:agendaItem', 'remove'],
+	emits: ['updateAgendaItem', 'remove'],
+
+	methods: {
+		setAgendaItemField(field, value)
+		{
+			this.$emit('updateAgendaItem', { field, value })
+		}
+	}
 };
 </script>
 
 <style scoped>
 .agenda-item-form {
-  border: 2px solid var(--blue-light);
-  border-radius: 8px;
-  position: relative;
-  padding: 20px 10% 0 16px;
+	border: 2px solid var(--blue-light);
+	border-radius: 8px;
+	position: relative;
+	padding: 20px 10% 0 16px;
 }
 
 .agenda-item-form__remove-button {
-  position: absolute;
-  top: 4px;
-  right: 0;
-  box-shadow: none;
-  border: none;
-  background-color: transparent;
-  outline: none;
-  padding: 4px;
-  cursor: pointer;
-  transition: 0.2s opacity;
+	position: absolute;
+	top: 4px;
+	right: 0;
+	box-shadow: none;
+	border: none;
+	background-color: transparent;
+	outline: none;
+	padding: 4px;
+	cursor: pointer;
+	transition: 0.2s opacity;
 }
 
 .agenda-item-form__remove-button:hover {
-  opacity: 0.6;
+	opacity: 0.6;
 }
 
 .agenda-item-form__row {
-  display: flex;
-  flex-direction: column;
+	display: flex;
+	flex-direction: column;
 }
 
-.agenda-item-form__col + .agenda-item-form__col {
-  margin-top: 16px;
+.agenda-item-form__col+.agenda-item-form__col {
+	margin-top: 16px;
 }
 
 .agenda-item-form__col:first-child {
-  margin-left: 0;
+	margin-left: 0;
 }
 
 @media all and (min-width: 992px) {
-  .agenda-item-form {
-    padding: 28px 25% 4px 24px;
-  }
+	.agenda-item-form {
+		padding: 28px 25% 4px 24px;
+	}
 
-  .agenda-item-form__remove-button {
-    top: 20px;
-    right: 20px;
-  }
+	.agenda-item-form__remove-button {
+		top: 20px;
+		right: 20px;
+	}
 
-  .agenda-item-form__row {
-    flex-direction: row;
-    justify-content: space-between;
-    margin: 0 -12px;
-  }
+	.agenda-item-form__row {
+		flex-direction: row;
+		justify-content: space-between;
+		margin: 0 -12px;
+	}
 
-  .agenda-item-form__col {
-    flex: 1 1 auto;
-    padding: 0 12px;
-  }
+	.agenda-item-form__col {
+		flex: 1 1 auto;
+		padding: 0 12px;
+	}
 
-  .agenda-item-form__col + .agenda-item-form__col {
-    margin-top: 0;
-  }
+	.agenda-item-form__col+.agenda-item-form__col {
+		margin-top: 0;
+	}
 
-  .agenda-item-form__col:first-child {
-    margin-left: 0;
-  }
+	.agenda-item-form__col:first-child {
+		margin-left: 0;
+	}
 }
 </style>
