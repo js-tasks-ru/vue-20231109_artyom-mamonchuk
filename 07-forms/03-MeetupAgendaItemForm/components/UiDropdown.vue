be1cf04cfd5fb82eb184a27150896f2d1dcec164
Artem <mart_22@ukr.net>
2023-12-28 13:23:32 +0200

    07-forms/03-MeetupAgendaItemForm: Failed tests

--- 07-forms/03-MeetupAgendaItemForm/components/UiDropdown.vue
+++ 07-forms/03-MeetupAgendaItemForm/components/UiDropdown.vue
@@ -1,215 +1,227 @@
 <template>
-  <div class="dropdown" :class="{ dropdown_opened: isOpen }">
-    <button type="button" class="dropdown__toggle" :class="{ dropdown__toggle_icon: hasIcons }" @click="toggleOpen">
-      <UiIcon v-if="selected?.icon" :icon="selected.icon" class="dropdown__icon" />
-      <span>{{ modelValue !== undefined ? selected?.text : title }}</span>
-    </button>
+	<div class="dropdown" :class="{ 'dropdown_opened': isOpened }" ref="dropDown">
+		<button type="button" class="dropdown__toggle" :class="{ 'dropdown__toggle_icon': existIcon }"
+			@click="toggleVisibilityDropdownMenu">
+			<UiIcon v-if="currentOption?.icon" :icon="currentOption.icon" class="dropdown__icon" />
+			<span>{{ currentOption?.text || title }}</span>
+		</button>
 
-    <div v-show="isOpen" class="dropdown__menu" role="listbox">
-      <button
-        v-for="option in options"
-        :key="option.value"
-        class="dropdown__item"
-        :class="{ dropdown__item_icon: hasIcons }"
-        role="option"
-        type="button"
-        @click="select(option.value)"
-      >
-        <UiIcon v-if="option.icon" :icon="option.icon" class="dropdown__icon" />
-        {{ option.text }}
-      </button>
-    </div>
-
-    <select v-model="selectModel" :name="name" style="display: none">
-      <option v-for="option in options" :key="option.value" :value="option.value">{{ option.text }}</option>
-    </select>
-  </div>
+		<transition name="dropdown">
+			<div class="dropdown__menu" role="listbox" v-show="isOpened">
+				<button class="dropdown__item"
+					:class="{ 'dropdown__item_icon': existIcon, 'selected': currentOption?.value === option?.value }"
+					role="option" type="button" v-for=" option in options " :key="option.value"
+					@click="selectOption(option.value)">
+					<UiIcon v-if="option?.icon" :icon="option.icon" class="dropdown__icon" />
+					{{ option.text }}
+				</button>
+			</div>
+		</transition>
+	</div>
 </template>
 
 <script>
 import UiIcon from './UiIcon.vue';
 
 export default {
-  name: 'UiDropdown',
+	name: 'UiDropdown',
 
-  components: { UiIcon },
+	components: { UiIcon },
 
-  props: {
-    modelValue: {},
+	mounted()
+	{
+		document.addEventListener('click', this.closeUiDropdown);
+	},
+	unmounted()
+	{
+		document.removeEventListener('click', this.closeUiDropdown);
+	},
 
-    options: {
-      type: Array,
-      required: true,
-      validator: (options) =>
-        options.every(
-          (option) => typeof option === 'object' && option !== null && 'value' in option && 'text' in option,
-        ),
-    },
+	data()
+	{
+		return {
+			isOpened: false
+		}
+	},
 
-    title: {
-      type: String,
-      required: true,
-    },
+	emits: ['update:modelValue'],
 
-    name: {
-      type: String,
-    },
-  },
-
-  emits: ['update:modelValue'],
-
-  data() {
-    return {
-      isOpen: false,
-    };
-  },
-
-  computed: {
-    selected() {
-      return this.options.find((option) => option.value === this.modelValue);
-    },
-
-    hasIcons() {
-      return this.options.some((option) => option.icon);
-    },
-
-    selectModel: {
-      get() {
-        return this.modelValue;
-      },
-
-      set(value) {
-        this.select(value);
-      },
-    },
-  },
-
-  methods: {
-    toggleOpen() {
-      this.isOpen = !this.isOpen;
-    },
-
-    select(value) {
-      this.isOpen = false;
-      this.$emit('update:modelValue', value);
-    },
-  },
+	props: {
+		modelValue: {
+			type: String,
+			default: ''
+		},
+		options: {
+			type: Array,
+			required: true,
+		},
+		title: {
+			type: String,
+			required: true
+		}
+	},
+	methods: {
+		toggleVisibilityDropdownMenu()
+		{
+			this.isOpened = !this.isOpened;
+		},
+		selectOption(optionValue)
+		{
+			this.isOpened = !this.isOpened;
+			this.$emit('update:modelValue', optionValue);
+		},
+		closeUiDropdown({ target })
+		{
+			if (!this.$refs.dropDown.contains(target)) {
+				this.isOpened = false;
+			}
+		}
+	},
+	computed: {
+		currentOption()
+		{
+			const [option] = this.options.filter(option => option.value === this.modelValue);
+			return option;
+		},
+		existIcon()
+		{
+			return this.options.some(option => Boolean(option.icon));
+		}
+	}
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .dropdown {
-  position: relative;
-  display: inline-block;
+	position: relative;
+	display: inline-block;
 }
 
 .dropdown__toggle {
-  display: inline-block;
-  background-color: var(--white);
-  background-position: calc(100% - 10px) calc(100% - 10px);
-  border: 2px solid var(--blue-light);
-  border-radius: 8px;
-  position: relative;
-  padding: 10px 56px 10px 24px;
-  font-weight: 500;
-  font-size: 20px;
-  line-height: 28px;
-  color: initial;
-  text-align: center;
-  transition-duration: 0.2s;
-  transition-property: background-color, fill, color;
-  outline: none;
-  box-shadow: none;
-  cursor: pointer;
-  text-decoration: none;
+	display: inline-block;
+	background-color: var(--white);
+	background-position: calc(100% - 10px) calc(100% - 10px);
+	border: 2px solid var(--blue-light);
+	border-radius: 8px;
+	position: relative;
+	padding: 10px 56px 10px 24px;
+	font-weight: 500;
+	font-size: 20px;
+	line-height: 28px;
+	color: initial;
+	text-align: center;
+	transition-duration: 0.2s;
+	transition-property: background-color, fill, color;
+	outline: none;
+	box-shadow: none;
+	cursor: pointer;
+	text-decoration: none;
+
+	&:after {
+		content: '';
+		position: absolute;
+		top: 50%;
+		right: 16px;
+		translate: 0 -50%;
+		background: url('@/assets/icons/icon-chevron-down.svg') no-repeat;
+		background-size: cover;
+		display: block;
+		width: 24px;
+		height: 24px;
+		transition: 0.2s transform;
+	}
 }
 
-.dropdown__toggle:after {
-  content: '';
-  position: absolute;
-  top: 15px;
-  right: 16px;
-  transform: none;
-  background: url('@/assets/icons/icon-chevron-down.svg') no-repeat;
-  background-size: cover;
-  display: block;
-  width: 24px;
-  height: 24px;
-  transition: 0.2s transform;
+
+.dropdown__toggle_icon {
+	padding-left: 56px;
 }
 
-.dropdown__toggle.dropdown__toggle_icon {
-  padding-left: 56px;
-}
+.dropdown_opened {
+	.dropdown__toggle {
+		border-color: var(--blue);
+		border-bottom-color: transparent;
+		border-bottom-left-radius: 0;
+		border-bottom-right-radius: 0;
 
-.dropdown_opened .dropdown__toggle {
-  border-color: var(--blue);
-  border-bottom-color: transparent;
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
-}
-
-.dropdown_opened .dropdown__toggle:after {
-  transform: rotate(180deg);
+		&:after {
+			transform: rotate(180deg);
+		}
+	}
 }
 
 .dropdown__menu {
-  margin: 0;
-  width: 100%;
-  padding: 0;
-  border-radius: 0 0 8px 8px;
-  left: 0;
-  z-index: 95;
-  background-clip: padding-box;
-  display: none;
-  flex-direction: column;
-  border: 2px solid var(--blue);
-  border-top: none;
-  overflow: hidden;
+	position: absolute;
+	top: calc(100% - 1px);
+	left: 0;
+	z-index: 95;
+	min-width: 100%;
+	border-radius: 0 0 8px 8px;
+	border: 2px solid var(--blue);
+	border-top: none;
+	bottom: auto;
+	display: flex;
+	flex-direction: column;
+	margin: 0;
+	padding: 0;
+	background-color: #fff;
+	overflow: hidden;
+	transform-origin: top;
+
 }
 
-.dropdown_opened .dropdown__menu {
-  display: flex;
-  position: absolute;
-  transform: translate3d(0px, 52px, 0px);
-  top: -1px;
-  left: 0;
-  will-change: transform;
-  right: auto;
-  bottom: auto;
+.dropdown-enter-active,
+.dropdown-leave-active {
+	opacity: 1;
+	visibility: visible;
+	transform: scaleY(1);
+	transition: all 0.3s cubic-bezier(.86, 0, .07, 1);
+}
+
+.dropdown-enter-from,
+.dropdown-leave-to {
+	opacity: 0;
+	visibility: hidden;
+	transform: scaleY(0);
 }
 
 .dropdown__item {
-  padding: 8px 16px;
-  font-weight: 500;
-  font-size: 20px;
-  line-height: 28px;
-  background-color: var(--white);
-  box-shadow: none;
-  border: none;
-  cursor: pointer;
-  text-align: left;
-  transition-duration: 0.2s;
-  transition-property: background-color, border-color, color;
-  outline: none;
-  text-decoration: none;
+	padding: 8px 16px 8px 24px;
+	font-weight: 500;
+	font-size: 20px;
+	line-height: 28px;
+	background-color: var(--white);
+	box-shadow: none;
+	border: none;
+	cursor: pointer;
+	text-align: left;
+	transition-duration: 0.2s;
+	transition-property: background-color, border-color, color;
+	outline: none;
+	text-decoration: none;
+
+	&:hover,
+	&:focus {
+		background-color: var(--grey-light);
+	}
+
+	&.selected {
+		background-color: var(--grey-2);
+	}
 }
 
-.dropdown__item:hover,
-.dropdown__item:focus {
-  background-color: var(--grey-light);
+.dropdown__item_icon {
+	padding-left: 56px;
+	position: relative;
 }
 
-.dropdown__item.dropdown__item_icon {
-  padding-left: 56px;
-  position: relative;
-}
-
-.dropdown__item.dropdown__item_icon .dropdown__icon,
-.dropdown__toggle_icon .dropdown__icon {
-  position: absolute;
-  top: 50%;
-  left: 16px;
-  transform: translate(0, -50%);
+.dropdown__item_icon,
+.dropdown__toggle_icon {
+	.dropdown__icon {
+		position: absolute;
+		top: 50%;
+		left: 16px;
+		transform: translate(0, -50%);
+	}
 }
 </style>
