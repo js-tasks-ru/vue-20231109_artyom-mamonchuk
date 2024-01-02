3dacb13963ff827b8fe6e6df8f848133f32d3664
Artem <mart_22@ukr.net>
2024-01-02 18:17:06 +0200

    10-slots/01-UiPaginationView: Solve task

--- 10-slots/01-UiPaginationView/App.vue
+++ 10-slots/01-UiPaginationView/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="sample container">
-    <div class="pagination-buttons">
-      <UiButton v-for="i in pagesCount" :key="i" :variant="page === i ? 'primary' : 'secondary'" @click="page = i">
-        {{ i }}
-      </UiButton>
-    </div>
+	<div class="sample container">
+		<div class="pagination-buttons">
+			<UiButton v-for="i in pagesCount" :key="i" :variant="page === i ? 'primary' : 'secondary'" @click="page = i">
+				{{ i }}
+			</UiButton>
+		</div>
 
-    <UiPaginationView v-slot="{ item }" :items="meetups" :page="page" :per-page="perPage">
-      <MeetupCard :meetup="item" class="list-item" />
-    </UiPaginationView>
-  </div>
+		<UiPaginationView v-slot="{ item }" :items="meetups" :page="page" :per-page="perPage">
+			<MeetupCard :meetup="item" class="list-item" />
+		</UiPaginationView>
+	</div>
 </template>
 
 <script>
@@ -19,34 +19,36 @@ import MeetupCard from './components/MeetupCard.vue';
 import meetups from './meetups.json';
 
 export default {
-  name: 'App',
+	name: 'App',
 
-  components: { MeetupCard, UiButton, UiPaginationView },
+	components: { MeetupCard, UiButton, UiPaginationView },
 
-  data() {
-    return {
-      page: 1,
-      perPage: 2,
-      meetups,
-    };
-  },
+	data()
+	{
+		return {
+			page: 1,
+			perPage: 2,
+			meetups,
+		};
+	},
 
-  computed: {
-    pagesCount() {
-      return Math.ceil(this.meetups.length / this.perPage);
-    },
-  },
+	computed: {
+		pagesCount()
+		{
+			return Math.ceil(this.meetups.length / this.perPage);
+		},
+	},
 };
 </script>
 
 <style scoped>
 .pagination-buttons {
-  display: flex;
-  gap: 1rem;
-  margin-bottom: 3rem;
+	display: flex;
+	gap: 1rem;
+	margin-bottom: 3rem;
 }
 
 .list-item {
-  margin: 0 0 32px 0;
+	margin: 0 0 32px 0;
 }
 </style>
--- 10-slots/01-UiPaginationView/components/UiPaginationView.vue
+++ 10-slots/01-UiPaginationView/components/UiPaginationView.vue
@@ -1,32 +1,43 @@
 <template>
-  <div class="pagination-container">
-    <!-- Контент страницы -->
-  </div>
+	<div class="pagination-container">
+		<slot v-for="item in currentPageData" :item="item" :key="item.id"></slot>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'UiPaginationView',
+	name: 'UiPaginationView',
 
-  props: {
-    page: {
-      type: Number,
-      required: true,
-      validator: (value) => value > 0,
-    },
+	props: {
+		page: {
+			type: Number,
+			required: true,
+			validator: (value) => value > 0,
+		},
 
-    perPage: {
-      type: Number,
-      required: true,
-      validator: (value) => value > 0,
-    },
+		perPage: {
+			type: Number,
+			required: true,
+			validator: (value) => value > 0,
+		},
 
-    items: {
-      type: Array,
-      required: true,
-    },
-  },
+		items: {
+			type: Array,
+			required: true,
+		},
+	},
+
+	computed: {
+		currentPageData()
+		{
+			const startFrom = (this.page - 1) * this.perPage;
+			const endTo = startFrom + this.perPage;
+
+			return this.items.slice(startFrom, endTo);
+		}
+	}
 };
 </script>
 
-<style></style>
+<style>
+</style>
