8c40eb1343637906db076778b23db5dee6ec9568
Artem <mart_22@ukr.net>
2024-01-08 22:59:48 +0200

    09-rendering/03-UiPanes-JSX: Solve task

--- 09-rendering/03-UiPanes-JSX/App.vue
+++ 09-rendering/03-UiPanes-JSX/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <div class="sample container">
-    <!-- Должны выводиться 4 панели без именованных слотов <template #pane-X> -->
-    <UiPanes>
-      <template #pane-0>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum harum id labore repellat sequi. Aliquam at
-          consequuntur dolorem doloremque dolorum eum excepturi, incidunt laudantium, molestias quidem reprehenderit
-          sunt suscipit ullam.
-        </div>
-      </template>
-      <template #pane-1>
-        <UiButton variant="primary">Pane 2 - Button</UiButton>
-      </template>
-      <template #pane-2>
-        <UiInput placeholder="Pane 3 - Input" />
-      </template>
-      <template #pane-3>
-        <div>
-          <UiIcon icon="search" />
-          <UiIcon icon="tv" />
-          <UiIcon icon="coffee" />
-          <UiIcon icon="key" />
-        </div>
-      </template>
-    </UiPanes>
-  </div>
+	<div class="sample container">
+		<UiPanes>
+			<template #default>
+				<div>
+					1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum harum id labore repellat sequi. Aliquam at
+					consequuntur dolorem doloremque dolorum eum excepturi, incidunt laudantium, molestias quidem reprehenderit
+					sunt suscipit ullam.
+				</div>
+				<UiButton variant="primary">Pane</UiButton>
+				<UiInput placeholder="Input" />
+				<div>
+					<UiIcon icon="search" />
+					<UiIcon icon="tv" />
+					<UiIcon icon="coffee" />
+					<UiIcon icon="key" />
+				</div>
+			</template>
+		</UiPanes>
+	</div>
 </template>
 
 <script>
@@ -34,10 +27,11 @@ import UiInput from './components/UiInput.vue';
 import UiIcon from './components/UiIcon.vue';
 
 export default {
-  name: 'App',
+	name: 'App',
 
-  components: { UiIcon, UiInput, UiButton, UiPanes },
+	components: { UiIcon, UiInput, UiButton, UiPanes },
 };
 </script>
 
-<style></style>
+<style>
+</style>
--- 09-rendering/03-UiPanes-JSX/components/UiPanes.vue
+++ 09-rendering/03-UiPanes-JSX/components/UiPanes.vue
@@ -1,118 +1,96 @@
-<template>
-  <div class="panes">
-    <!--  Pane 0 -->
-    <div class="pane">
-      <div class="pane__content">
-        <!-- Определяем, какой именно слот рендерить по массиву порядка панелей -->
-        <slot :name="`pane-${panes[0]}`" />
-      </div>
-      <div class="pane__controls">
-        <!-- Скрываем кнопку классом pane__disabled-button -->
-        <UiButton class="pane__disabled-button" variant="secondary" block @click="up(0)"> Up </UiButton>
-        <UiButton variant="danger" block @click="down(0)"> Down </UiButton>
-      </div>
-    </div>
-    <!--  Pane 1 -->
-    <div class="pane">
-      <div class="pane__content">
-        <slot :name="`pane-${panes[1]}`" />
-      </div>
-      <div class="pane__controls">
-        <UiButton variant="secondary" block @click="up(1)"> Up </UiButton>
-        <UiButton variant="danger" block @click="down(1)"> Down </UiButton>
-      </div>
-    </div>
-    <!--  Pane 2 -->
-    <div class="pane">
-      <div class="pane__content">
-        <slot :name="`pane-${panes[2]}`" />
-      </div>
-      <div class="pane__controls">
-        <UiButton variant="secondary" block @click="up(2)"> Up </UiButton>
-        <UiButton class="pane__disabled-button" variant="danger" block @click="down(2)"> Down </UiButton>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script lang="jsx">
-// Предлагается решать задачу с использованием JSX, но вы можете использовать и чистые рендер-функции
-
 import UiButton from './UiButton.vue';
 
 export default {
-  name: 'UiPanes',
+	name: 'UiPanes',
 
-  components: {
-    UiButton,
-  },
+	data()
+	{
+		return {
+			panes: this.$slots.default?.(),
+		};
+	},
 
-  data() {
-    return {
-      /**
-       * Массив с текущим порядком номеров моделей, например
-       * [0, 1, 2]
-       * @type {number[]|null}
-       */
-      panes: [0, 1, 2],
-      // Сейчас здесь массив ровно из трёх элементов, но решение должно быть универсальным для любого количества узлов
-    };
-  },
+	render()
+	{
+		return (
+			<div class="panes">
+				{
+					this.panes.map((pane, index) =>
+					(
+						<div class="pane">
+							<div class="pane__content">
+								{pane}
+							</div>
+							<div class="pane__controls">
+								<UiButton
+									variant="secondary"
+									block
+									onClick={() => this.up(index)}
+									class={{ 'pane__disabled-button': index === 0 }}>
+									Up
+								</UiButton>
+								<UiButton
+									variant="danger"
+									block
+									onClick={() => this.down(index)}
+									class={{ 'pane__disabled-button': index + 1 === this.panes.length }}>
+									Down
+								</UiButton>
+							</div>
+						</div>
+					))
+				}
+			</div>
+		)
+	},
 
-  methods: {
-    /**
-     * Переместить i-ую панель вверх
-     *
-     * @param {number} i
-     */
-    up(i) {
-      const temp = this.panes[i];
-      this.panes[i] = this.panes[i - 1];
-      this.panes[i - 1] = temp;
-    },
+	methods: {
 
-    /**
-     * Переместить i-ую панель вниз
-     *
-     * @param {number} i
-     */
-    down(i) {
-      const temp = this.panes[i];
-      this.panes[i] = this.panes[i + 1];
-      this.panes[i + 1] = temp;
-    },
-  },
+		up(index)
+		{
+			const currentPane = this.panes[index];
+			this.panes[index] = this.panes[index - 1];
+			this.panes[index - 1] = currentPane;
+		},
+		down(index)
+		{
+			const currentPane = this.panes[index];
+			this.panes[index] = this.panes[index + 1];
+			this.panes[index + 1] = currentPane;
+		},
+	},
 };
 </script>
 
 <style scoped>
 .panes {
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
 }
 
 .pane {
-  display: flex;
-  flex-direction: row;
-  border: 3px solid var(--blue-light);
+	display: flex;
+	flex-direction: row;
+	border: 3px solid var(--blue-light);
 }
 
 .pane__content {
-  border-right: 3px solid var(--blue-light);
-  padding: 8px;
-  flex: 1 0;
+	border-right: 3px solid var(--blue-light);
+	padding: 8px;
+	flex: 1 0;
 }
 
 .pane__controls {
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  padding: 8px;
-  width: min-content;
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
+	padding: 8px;
+	width: min-content;
 }
 
 .pane__disabled-button {
-  visibility: hidden;
+	visibility: hidden;
 }
 </style>
