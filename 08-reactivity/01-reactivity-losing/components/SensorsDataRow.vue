6e05faf870bb064201b9a9d69e03f3871dde4a6b
Artem <mart_22@ukr.net>
2024-01-11 17:11:46 +0200

    08-reactivity/01-reactivity-losing: Solve task

--- 08-reactivity/01-reactivity-losing/components/SensorsDataRow.vue
+++ 08-reactivity/01-reactivity-losing/components/SensorsDataRow.vue
@@ -1,40 +1,37 @@
 <template>
-  <div class="sensor-data">
-    <div>#{{ sensor.id }}</div>
-    <div>
-      <span class="sensor-data__label">{{ sensor.label }}: </span>
-      <span class="sensor-data__value">{{ sensor.value.toFixed(1) }}</span>
-      <span class="sensor-data__unit">{{ sensor.unit }}</span>
-    </div>
-  </div>
+	<div class="sensor-data">
+		<div>#{{ sensor.id }}</div>
+		<div>
+			<span class="sensor-data__label">{{ sensor.label }}: </span>
+			<span class="sensor-data__value">{{ sensor.value.toFixed(1) }}</span>
+			<span class="sensor-data__unit">{{ sensor.unit }}</span>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'SensorsDataRow',
+	name: 'SensorsDataRow',
 
-  props: {
-    sensor: Object,
-  },
+	props: {
+		sensor: Object,
+	},
 };
 </script>
 
 <style>
 .sensor-data {
-  display: flex;
-  gap: 1rem;
-  font-family: monospace;
-  font-size: 16px;
-}
-
-.sensor-data__label {
+	display: flex;
+	gap: 1rem;
+	font-family: monospace;
+	font-size: 16px;
 }
 
 .sensor-data__value {
-  font-weight: bold;
+	font-weight: bold;
 }
 
 .sensor-data__unit {
-  color: #999999;
+	color: #999999;
 }
 </style>
